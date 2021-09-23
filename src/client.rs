70be4f5f4db44203cfbd31ed1ef9c266f5c46a5a
Matan Kushner <hello@matchai.dev>
2021-09-23 18:22:16 +0100

    Process as many requests as there are plugins

--- src/client.rs
+++ src/client.rs
@@ -1,7 +1,7 @@
 use anyhow::anyhow;
 use service::{init_tracing, PluginClient};
 use std::{env::args, net::Ipv6Addr};
-use tarpc::{client, context};
+use tarpc::{client, context, serde_transport::tcp};
 use tokio_serde::formats::Bincode;
 
 #[tokio::main]
@@ -9,15 +9,18 @@ async fn main() -> anyhow::Result<()> {
     init_tracing("Starship Example Plugin")?;
 
     let port = args().nth(1).ok_or_else(|| anyhow!("Port required."))?;
-    let server_addr = (Ipv6Addr::LOCALHOST, port.parse()?);
-    let transport = tarpc::serde_transport::tcp::connect(server_addr, Bincode::default);
+    let addr = (Ipv6Addr::LOCALHOST, port.parse()?);
 
-    let client = PluginClient::new(client::Config::default(), transport.await?).spawn();
-    let output = client
+    let transport = tcp::connect(addr, Bincode::default).await?;
+    let client = PluginClient::new(client::Config::default(), transport).spawn();
+
+    client
         .hello(context::current(), "matchai".to_string())
         .await?;
 
-    tracing::info!("{:?}", output);
+    client
+        .hello(context::current(), "matchai2".to_string())
+        .await?;
 
     Ok(())
 }
