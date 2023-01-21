ccf7811d87b35be399c01091b0979830d7dd7949
Matan Kushner <hello@matchai.dev>
2023-01-21 21:48:41 +0900

    Convert server to be long-running

--- src/server.rs
+++ src/server.rs
@@ -4,7 +4,7 @@ use std::{env, net::SocketAddr, path::PathBuf, process::Stdio};
 use tarpc::{
     context,
     serde_transport::tcp,
-    server::{incoming::Incoming, BaseChannel},
+    server::{incoming::Incoming, BaseChannel, Channel},
 };
 use tokio::process::Command;
 use tokio_serde::formats::Bincode;
@@ -25,38 +25,26 @@ impl Plugin for PluginServer {
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     init_tracing()?;
-
-    let socket_addr = spawn_plugin_server().await?;
-    let server_port = socket_addr.port().to_string();
-
-    let mut plugin_handles = Vec::with_capacity(10);
-    for _ in 1..10 {
-        let mut child = Command::new("./target/debug/starship-plugin-directory")
-            .arg(&server_port)
-            .stdin(Stdio::null())
-            .spawn()?;
-
-        plugin_handles.push(tokio::spawn(async move { child.wait().await }));
-    }
-
-    future::join_all(plugin_handles).await;
+    spawn_plugin_server().await?;
 
     Ok(())
 }
 
 /// Spawn an instance of the Starship plugin server, providing the socket address
 /// for clients to connect to.
-async fn spawn_plugin_server() -> anyhow::Result<SocketAddr> {
-    let listener = tcp::listen("localhost:0", Bincode::default)
-        .await?
-        .filter_map(|r| async { r.ok() });
+async fn spawn_plugin_server() -> anyhow::Result<()> {
+    let listener = tcp::listen("localhost:0", Bincode::default).await?;
 
-    let socket_addr = listener.get_ref().local_addr();
+    let socket_addr = listener.local_addr();
+    println!("Listening on {}", socket_addr);
 
-    let plugin_server = listener
+    listener
+        .filter_map(|r| future::ready(r.ok()))
         .map(BaseChannel::with_defaults)
-        .execute(PluginServer.serve());
-    tokio::spawn(plugin_server);
+        .map(|channel| channel.execute(PluginServer.serve()))
+        .buffer_unordered(10)
+        .for_each(|_| async {})
+        .await;
 
-    Ok(socket_addr)
+    Ok(())
 }
