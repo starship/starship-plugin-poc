5affc0795037a5df82134f8bbec1d1afb13ae5fe
Matan Kushner <hello@matchai.dev>
2023-07-08 20:04:10 -0400

    Begin adding daemon sockets

--- crates/daemon/src/main.rs
+++ crates/daemon/src/main.rs
@@ -1,9 +1,39 @@
+use std::fs;
+use std::path::PathBuf;
+
 use anyhow::Result;
+use once_cell::sync::Lazy;
 use plugin_runtime::Runtime;
+use shellexpand::tilde;
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpListener;
 
 mod plugin_runtime;
+
+pub static CACHE_DIR: Lazy<PathBuf> =
+    Lazy::new(|| PathBuf::from(tilde("~/.cache/starship").into_owned()));
+pub static CONFIG_DIR: Lazy<PathBuf> =
+    Lazy::new(|| PathBuf::from(tilde("~/.config/starship").into_owned()));
+pub static DATA_DIR: Lazy<PathBuf> =
+    Lazy::new(|| PathBuf::from(tilde("~/.local/share/starship").into_owned()));
+
+pub static SOCKET_PATH: Lazy<PathBuf> = Lazy::new(|| DATA_DIR.join("starship.sock"));
+
+fn _init_directories() -> Result<()> {
+    if !DATA_DIR.exists() {
+        fs::create_dir_all(DATA_DIR.as_path())?;
+    }
+
+    if !CONFIG_DIR.exists() {
+        fs::create_dir_all(CONFIG_DIR.as_path())?;
+    }
+
+    if !CACHE_DIR.exists() {
+        fs::create_dir_all(CACHE_DIR.as_path())?;
+    }
+
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
